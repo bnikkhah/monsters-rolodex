71ca305952b08ff3579b3ca70a42194d2fbe3500
Behnam Nikkhah <bnikkhah22@gmail.com>
2023-01-08 16:55:11 -0700

    convert to typescript

---
+++ src/components/card-list/card-list.component.tsx
@@ -0,0 +1,17 @@
+import { Monster } from '../../App';
+import './card-list.styles.css'
+import Card from "../card/card.component";
+
+type CardListProps = {
+    monsters: Monster[]
+}
+
+const CardList = ({ monsters }: CardListProps) => {
+    return (
+        <div className="card-list">
+            {monsters.map((monster) => <Card monster={monster} key={monster.id} />)}
+        </div>
+    )
+}
+
+export default CardList
